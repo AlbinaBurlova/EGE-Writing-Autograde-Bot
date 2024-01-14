7594b00da9d489767b8725228b581d0f8e521212
Planca <117646051+AlbinaBurlova@users.noreply.github.com>
2024-01-14 23:00:42 +0700

    add developers' ids

--- config_reader.py
+++ config_reader.py
@@ -5,6 +5,8 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
 
     bot_token: SecretStr
+    developer_id_1: int
+    developer_id_2: int
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
