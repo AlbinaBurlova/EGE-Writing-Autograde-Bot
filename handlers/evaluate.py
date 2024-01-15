0416c0954aeb9c89ee61088b99126044305aa2e5
Planca <117646051+AlbinaBurlova@users.noreply.github.com>
2024-01-16 03:03:38 +0700

    add some strings

--- handlers/evaluate.py
+++ handlers/evaluate.py
@@ -1,4 +1,5 @@
 from typing import Union
+import asyncio
 
 from aiogram import Router, types, F
 from aiogram.types import CallbackQuery
@@ -8,7 +9,9 @@ from aiogram.filters import Command, StateFilter
 
 from fast_api.api_functions import send_to_api
 from handlers.start import create_inline_kb
-from utils.strings import LETTER_ONE, LETTER_TWO, LETTER_THREE, ERROR, RESTART_MESSAGE, WAITING_MESSAGE
+from utils.strings import LETTER_ONE, LETTER_TWO, LETTER_THREE, RESTART_MESSAGE, WAITING_MESSAGE, TRY_AGAIN
+from handlers.get_stat import results
+
 
 router = Router()
 
@@ -41,47 +44,43 @@ async def process_text_input(message: types.Message, state: FSMContext):
     chosen_letter = user_data['chosen_letter']
     await state.update_data(user_text=message.text)
 
-    # Подключение к API
-    evaluation_result = await send_to_api(message.text, letter=chosen_letter)
-
-    keyboard = create_inline_kb(1, 'btn_6', 'btn_7', 'btn_8', 'btn_9')
-
-    if evaluation_result == ERROR:
-        await message.answer(evaluation_result, reply_markup=keyboard)
-    else:
-        await message.answer(f"Ваш результат:\nРешение коммуникативной задачи: {evaluation_result} из 2 баллов",
+    try:
+        # Подключение к API
+        evaluation_result = await send_to_api(message.text, letter=chosen_letter)
+        await state.update_data(result=evaluation_result)
+        results.append(evaluation_result)
+        keyboard = create_inline_kb(1, 'btn_6', 'btn_7', 'btn_8', 'btn_9')
+        await message.answer(f"Ваш результат: {evaluation_result['total']} из 6 баллов",
                              reply_markup=keyboard)
+
+    except Exception as e:
+        await message.answer(TRY_AGAIN)
+        await asyncio.sleep(3)
+        await start_over(message)
+        await state.set_state(state=None)
+        return
 
     await state.set_state(state=None)
 
 
 @router.callback_query(F.data == 'get_comments')
-async def get_comments(callback: CallbackQuery):
-    await callback.message.answer(text='Здесь будут выведены комментарии')
-
-
-@router.callback_query(F.data == 'view_my_work')
-async def view_my_work(callback: CallbackQuery, state: FSMContext):
-
+async def get_comments(callback: CallbackQuery, state: FSMContext):
     user_data = await state.get_data()
-    user_text = user_data['user_text']
+    evaluation_result = user_data['result']
+    print(evaluation_result)
 
-    await callback.message.answer(text=f'Ваша работа: \n{user_text}')
+    await callback.message.answer(text=f'Решение коммуникативной задачи: {evaluation_result["k1"]} из 2 баллов.\nОрганизация текста: {evaluation_result["k2"]} из 2 баллов.\nЯзыковое оформление текста: {evaluation_result["k3"]} из 2 баллов.\n{evaluation_result["comments"]}')
 
 
 async def start_over(event: Union[types.Message, CallbackQuery]):
 
     keyboard = create_inline_kb(3, 'btn_1', 'btn_2', 'btn_3')
+    await event.bot.send_message(
+        chat_id=event.from_user.id,
+        text=RESTART_MESSAGE,
+        reply_markup=keyboard
+    )
 
-    if isinstance(event, types.Message):
-        await event.answer(
-            text=RESTART_MESSAGE,
-            reply_markup=keyboard
-        )
-    elif isinstance(event, CallbackQuery):
-        await event.message.answer(
-            text=RESTART_MESSAGE,
-            reply_markup=keyboard
-        )
 router.message(Command("evaluate"))(start_over)
 router.callback_query(F.data == 'restart')(start_over)
+
