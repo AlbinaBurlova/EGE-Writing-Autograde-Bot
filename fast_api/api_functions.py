7c389b915443c70e8994de36491b140e4080d6a8
Planca <117646051+AlbinaBurlova@users.noreply.github.com>
2024-01-16 03:07:28 +0700

    update api_functions.py

--- fast_api/api_functions.py
+++ fast_api/api_functions.py
@@ -1,24 +1,22 @@
 import httpx
 
-from utils.strings import ERROR, WAIT_FOR_TOO_LONG
+from utils.strings import ERROR
+
 
 async def send_to_api(text, letter):
 
-    url = "https://d5d08u0j2sectp2kvlgr.apigw.yandexcloud.net/predict"
+    url = "https://stunning-star-octopus.ngrok-free.app/predict"
 
     data = {
         "data": {
-            "Question": text,
-            "Text": letter
+            "Question": letter,
+            "Text": text
         }
     }
 
-    try:
-        async with httpx.AsyncClient(timeout=60.0) as client:
-            response = await client.post(url, json=data)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return ERROR
-    except (httpx.ConnectTimeout, httpx.ReadTimeout):
-        return WAIT_FOR_TOO_LONG
+    async with httpx.AsyncClient(timeout=60.0) as client:
+        response = await client.post(url, json=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return ERROR
