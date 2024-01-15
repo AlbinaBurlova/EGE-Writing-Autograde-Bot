2c576e95b7847e19b55bd1964e5ef11253cda2f7
Planca <117646051+AlbinaBurlova@users.noreply.github.com>
2024-01-15 22:12:38 +0700

    Update api_functions.py

--- fast_api/api_functions.py
+++ fast_api/api_functions.py
@@ -1,7 +1,6 @@
 import httpx
 
-from utils.strings import ERROR
-
+from utils.strings import ERROR, WAIT_FOR_TOO_LONG
 
 async def send_to_api(text, letter):
 
@@ -14,10 +13,12 @@ async def send_to_api(text, letter):
         }
     }
 
-    async with httpx.AsyncClient(timeout=30.0) as client:
-        response = await client.post(url, json=data)
-
-    if response.status_code == 200:
-        return response.json()["k1"]
-    else:
-        return ERROR
+    try:
+        async with httpx.AsyncClient(timeout=60.0) as client:
+            response = await client.post(url, json=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return ERROR
+    except (httpx.ConnectTimeout, httpx.ReadTimeout):
+        return WAIT_FOR_TOO_LONG
